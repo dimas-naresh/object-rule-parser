c067446c5382198448b6d96c61f1a30b8c47557a
d.daisubilissalannar <d.daisubilissalannar@homecredit.co.id>
2025-09-25 12:24:55 +0700

    Initialize project

---
+++ src/main/java/dimasnaresh/lib/objectruleparser/ObjectRuleParserApplication.java
@@ -0,0 +1,13 @@
+package dimasnaresh.lib.objectruleparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectRuleParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectRuleParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dimasnaresh/lib/objectruleparser/ObjectRuleParserApplicationTests.java
@@ -0,0 +1,13 @@
+package dimasnaresh.lib.objectruleparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectRuleParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
