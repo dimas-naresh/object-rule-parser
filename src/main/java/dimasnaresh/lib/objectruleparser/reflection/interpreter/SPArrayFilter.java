70c1708164917f9c55ed7ff30159e51bb4bd43b4
d.daisubilissalannar <d.daisubilissalannar@homecredit.co.id>
2025-09-26 06:11:20 +0700

    Add array translator in SPArrayFilter, make sure PersistentBag is safely to consumed

--- src/main/java/dimasnaresh/lib/objectruleparser/reflection/interpreter/SPArrayFilter.java
+++ src/main/java/dimasnaresh/lib/objectruleparser/reflection/interpreter/SPArrayFilter.java
@@ -1,5 +1,6 @@
 package dimasnaresh.lib.objectruleparser.reflection.interpreter;
 
+import org.hibernate.collection.spi.PersistentBag;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 import java.util.ArrayList;
@@ -16,17 +17,39 @@ public class SPArrayFilter extends Expression {
 
     @Override
     public Object invokeExpression() throws IllegalAccessException {
-        ArrayList<?> value = (ArrayList<?>) getValue();
+        ArrayList<?> value = translateArrayList(getValue());
+
         IllegalStateException stEx = new IllegalStateException();
+
         Object filteredValue = value.stream().filter(o -> {
             org.springframework.expression.ExpressionParser sprExpressionParser = new SpelExpressionParser();
             org.springframework.expression.Expression sprExpression = sprExpressionParser.parseExpression(getParams()[0]);
-            return (boolean) sprExpression.getValue(o);
+            Object expressionValue = sprExpression.getValue(o);
+
+            if (expressionValue != null)
+                return (boolean) expressionValue;
+
+            return false;
         }).findAny().orElse(null);
 
         if (stEx.getStackTrace() == null)
-            throw new IllegalAccessException("cannot found field ".concat(getParams()[0]).concat("on array ").concat(value.getClass().getName()));
+            throw new IllegalAccessException("cannot found field "
+                    .concat(getParams()[0]).concat("on array ")
+                    .concat(value.getClass().getName()));
         else
             return filteredValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> ArrayList<T> translateArrayList(Object array) {
+        try {
+            return (ArrayList<T>) array;
+        } catch (ClassCastException e) {
+            if (array instanceof PersistentBag<?>) {
+                return new ArrayList<>((PersistentBag) array);
+            } else {
+                throw e;
+            }
+        }
     }
 }
--- src/test/java/dimasnaresh/lib/objectruleparser/reflection/interpreter/SPArrayFilterTest.java
+++ src/test/java/dimasnaresh/lib/objectruleparser/reflection/interpreter/SPArrayFilterTest.java
@@ -1,14 +1,29 @@
 package dimasnaresh.lib.objectruleparser.reflection.interpreter;
 
 import dimasnaresh.lib.objectruleparser.param.TestingClassInvoking;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class SPArrayFilterTest {
     @Test
-    void invokeExpression() throws IllegalAccessException{
+    void invokeExpression_FilterArrayIndex2() throws IllegalAccessException{
+        List<TestingClassInvoking> arrayClassNumberOne = getTestingClassInvokings();
+        Expression expression = new SPArrayFilter(arrayClassNumberOne, "arr{fieldNumberOne == 'code_number_two'}");
+
+        TestingClassInvoking filteredObject = (TestingClassInvoking) expression.invokeExpression();
+
+        Assertions.assertNotNull(filteredObject);
+        Assertions.assertEquals("code_number_two", filteredObject.getFieldNumberOne());
+        Assertions.assertEquals("field_two_at_class_number_two", filteredObject.getFieldNumberTwo());
+        Assertions.assertEquals("field_three_at_class_number_two", filteredObject.getFieldNumberThree());
+    }
+
+    private static List<TestingClassInvoking> getTestingClassInvokings() {
         List<TestingClassInvoking> arrayClassNumberOne = new ArrayList<>();
         TestingClassInvoking numberOne = new TestingClassInvoking();
         TestingClassInvoking numberOne1 = new TestingClassInvoking();
@@ -21,13 +36,36 @@ class SPArrayFilterTest {
         numberOne1.setFieldNumberThree("field_three_at_class_number_two");
         arrayClassNumberOne.add(numberOne);
         arrayClassNumberOne.add(numberOne1);
-        Expression expression = new SPArrayFilter(arrayClassNumberOne, "arr{fieldNumberOne == 'code_number_two'}");
+        return arrayClassNumberOne;
+    }
 
-        TestingClassInvoking filteredObject = (TestingClassInvoking) expression.invokeExpression();
-        System.out.println(filteredObject.getFieldNumberOne().concat(", ")
-                .concat(filteredObject.getFieldNumberTwo())
-                .concat(", ").concat(filteredObject.getFieldNumberThree()));
+    @Test
+    void testFilterMatchFound() throws Exception {
+        TestingClassInvoking obj1 = new TestingClassInvoking();
+        obj1.setFieldNumberOne("yes");
+        TestingClassInvoking obj2 = new TestingClassInvoking();
+        obj2.setFieldNumberOne("no");
 
+        ArrayList<TestingClassInvoking> list = new ArrayList<>(Arrays.asList(obj1, obj2));
 
+        Expression filter = new SPArrayFilter(list, "arr{fieldNumberOne == 'yes'}");
+        Object result = filter.invokeExpression();
+
+        Assertions.assertNotNull(result);
+        Assertions.assertInstanceOf(TestingClassInvoking.class, result);
+        Assertions.assertEquals("yes", ((TestingClassInvoking) result).getFieldNumberOne());
+    }
+
+    @Test
+    void testFilterNoMatch() throws Exception {
+        TestingClassInvoking obj = new TestingClassInvoking();
+        obj.setFieldNumberOne("no");
+
+        ArrayList<TestingClassInvoking> list = new ArrayList<>(Collections.singletonList(obj));
+
+        Expression filter = new SPArrayFilter(list, "arr{fieldNumberOne == 'yes'}");
+        Object result = filter.invokeExpression();
+
+        Assertions.assertNull(result); // no match found
     }
 }
